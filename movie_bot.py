14c8d7dd2db455cbef1d4303cc4fae99fbae7c0b
mrjavohirjon <temostudios@gmail.com>
2026-02-07 23:28:49 +0500

    Update movie_bot.py

--- movie_bot.py
+++ movie_bot.py
@@ -23,7 +23,7 @@ req_col = db.requests
 
 API_ID = 38119035
 API_HASH = "0f84597433eacb749fd482ad238a104e"
-BOT_TOKEN = "5449865657:AAHm5lhjyYiieEdW1fpJTIde2D_daOlFJKc"
+BOT_TOKEN = "8555382121:AAFlAbCRF1yWGn76pIPlZSarI1JkjKYKgS8"
 
 MOVIE_CHANNEL = "@hshhshshshdgegeuejje"
 MANDATORY_CHANNEL = "@TG_Manager_uz"
